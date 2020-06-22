803b3212a06fe81dff8eae24d03d95533ffb66d2
esmith-33 <local4554@gmail.com>
2020-06-22 18:41:52 -0500

    update

--- web-start/public/scripts/main.js
+++ web-start/public/scripts/main.js
@@ -287,6 +287,8 @@ function toggleButton() {
 function checkSetup() {
   if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
     window.alert('You have not configured and imported the Firebase SDK. ' +
+      'Make sure you go through the codelab setup instructions and make ' +
+      'sure you are running the codelab using `firebase serve`');
   }
 }
 
